5a9ee66573dc6629c34ea7ce49591df75199447f
Peter Kinget <46092360+peterkinget@users.noreply.github.com>
2025-07-17 15:04:12 -0400

    manually ported a few sky130 cells

---
+++ designs/libs/gf180mcu_fd_sc_mcu9t5v0_symbols/sky130_stdcells_to_gf180mcu/do_text_replacements.py
@@ -0,0 +1,24 @@
+# run with python do_text_replacements.py
+ 
+import pathlib
+
+# List of replacements: (old_text, new_text)
+replacements = [
+    ("prefix=sky130_fd_sc_hd__", "prefix=gf180mcu_fd_sc_mcu9t5v0__"),
+    ("verilogprefix=sky130_fd_sc_hd__", "verilogprefix=gf180mcu_fd_sc_mcu9t5v0__"),
+    ("VGND=VGND VNB=VNB VPB=VPB VPWR=VPWR", "VGND=VSSd VNB=VDDd VPB=VSSd VPWR=VDDd"),
+    ( "@VGND @VNB @VPB @VPWR", "@VPWR @VNB @VPB @VGND")
+]
+
+# Process all .sym files in current dir and subdirectories
+sym_files = pathlib.Path('.').rglob('nand2*.sym')
+
+for file in sym_files:
+    text = file.read_text()
+    new_text = text
+    for old, new in replacements:
+        new_text = new_text.replace(old, new)
+
+    if new_text != text:
+        file.write_text(new_text)
+        print(f"Updated: {file}")
